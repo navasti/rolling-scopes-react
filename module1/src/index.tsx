7208449dc6ded23c0698da240518e2fe8921cd60
Pawel Szostak <pawel.szostak@itechart-group.com>
2022-07-04 14:53:56 +0200

    init: CRA project setup

---
+++ module1/src/index.tsx
@@ -0,0 +1,13 @@
+import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import React from 'react';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals(console.log);
