5ba5a23eab9e07d7652f730d367347363cbd08e9
Pawel Szostak <pawel.szostak@itechart-group.com>
2022-07-07 14:46:52 +0200

    test: adjust tests for new project structure

---
+++ src/modules/SearchPage/tests/resources.js
@@ -0,0 +1,25 @@
+export const isLoading = false;
+export const pokemons = [
+  {
+    abilities: [{ ability: { name: 'ability' }, is_hidden: false, slot: 2 }],
+    stats: [{ base_stat: 33, effort: 33, stat: { name: 'test' } }],
+    sprites: { back_default: 'url', front_default: 'url' },
+    types: [{ slot: 2, type: { name: 'test' } }],
+    name: 'Testing pokemon',
+    base_experience: 10,
+    height: 10,
+    weight: 10,
+    id: 34,
+  },
+  {
+    abilities: [{ ability: { name: 'ability2' }, is_hidden: true, slot: 1 }],
+    stats: [{ base_stat: 22, effort: 22, stat: { name: 'test' } }],
+    sprites: { back_default: 'url2', front_default: 'url2' },
+    types: [{ slot: 2, type: { name: 'test2' } }],
+    name: 'Testing pokemon2',
+    base_experience: 16,
+    height: 11,
+    weight: 11,
+    id: 35,
+  },
+];
--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -4,3 +4,5 @@ export { Layout } from './Layout';
 export { Header } from './Header';
 export { NotFound } from './NotFound';
 export { SearchPage } from './SearchPage';
+export { LocationBadge } from './Header/components';
+export { Card, SearchBar } from './SearchPage/components';
